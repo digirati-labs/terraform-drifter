e965d81e18cf88f63641be7936e47dec0ff3c988
Adam Christie <fractos@gmail.com>
2019-08-29 16:08:03 +0100

    initial commit.

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "slack_webhook_url" {}
+variable "terraform_s3_bucket" {}
+variable "terraform_s3_key" {}
+variable "terraform_github_repo" {}
+
+variable "terraform_github_branch" {
+  default = "master"
+}
+
+variable "terraform_github_folder" {
+  default = ""
+}
+
+variable "terraform_github_token" {}
+variable "cloudwatch_namespace" {}
+
+variable "tmp_folder" {
+  default = "/tmp"
+}
+
+variable "log_group_name" {}
+variable "prefix" {}
+variable "region" {}
+
+variable "drifter_docker_image" {
+  default = "digirati/drifter:latest"
+}
+
+variable "account_id" {}
+variable "cluster_id" {}
+variable "cron_expression" {}
